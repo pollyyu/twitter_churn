68e90cc064f848bd313bd6502959ec3bc19cffe9
pollyyu <paulynnyu@gmail.com>
2019-11-13 00:24:09 -0800

    Update config.py

--- tweepy_api/API_calls/config.py
+++ tweepy_api/API_calls/config.py
@@ -9,9 +9,9 @@ def create_api():
     '''
     # Authenticate to Twitter
 
-    auth = tweepy.OAuthHandler("5SDCerBHlWzSBAzvBXonhZ6Xe", "8zeJxu7srcQN6uhQAvcsWXHM8q8pVdzys6g6kfyeQhyVLcdcIR")
-    auth.set_access_token("2263992234-pp4lcAeHd7sYOOrlbjKfCPea7BC4vBr1xZoifce",
-        "RRcgaj400e2ubOFuiy8YwrvTBsjxs19YVhQB1tFYcN5lA")
+    auth = tweepy.OAuthHandler("XXXX", "XXXX")
+    auth.set_access_token("CONSUMER_KEY", "CONSUMER_SECRET",
+        "ACCESS_TOKEN","ACCESS_TOKEN_SECRET")
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, parser=tweepy.parsers.JSONParser())
 
     return api
